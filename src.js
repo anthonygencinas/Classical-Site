71a97dd746ed13de12b22cc86918e2dad604bb22
Anthony Encinas <anthonygencinas@gmail.com>
2019-01-07 18:26:02 -0700

    Add files via upload

---
+++ src.js
@@ -0,0 +1,12 @@
+/*
+    This file is a series of functions created to help with the website
+ */
+
+
+/*
+    A function to return back to the home screen
+ */
+function home(){
+    window.location.href = 'index.html';
+    console.log("went home");
+}
